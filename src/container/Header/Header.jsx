449a83d3950501edf5db5053b2404ca538679e50
robcabobb <robcabobb@gmail.com>
2022-05-13 14:50:24 -0400

    finished about section

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -24,9 +24,9 @@ const Header = () => (
 			</button>
 		</div>
 
-  <div className=""></div>
-
-		<div className="app__wrapper_img"></div>
+		<div className="app__wrapper_img">
+			<img src={images.welcome} alt="header_img" />
+		</div>
 	</div>
 );
 

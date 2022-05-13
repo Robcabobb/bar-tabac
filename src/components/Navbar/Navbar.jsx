42727bbb88082a0878f0fc9b0c1e8834704d5298
robcabobb <robcabobb@gmail.com>
2022-05-13 12:40:06 -0400

    started on header

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,48 +1,17 @@
-import React from "react";
+import { useState } from "react";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { MdOutlineRestaurantMenu } from "react-icons/md";
-
-import images from "../../constants/images";
-
 import "./Navbar.css";
 
-const Navbar = () => (
-	<nav className="app__navbar">
-		<div className="app__navbar-title">
-			<h1>Le COURTOISVILLE</h1>
-		</div>
-		<ul className="app__navbar-links">
-			<li className="p__opensans">
-				<a href="#home">Accueil</a>
-			</li>
-			<li className="p__opensans">
-				<a href="#about">Présentation</a>
-			</li>
-			<li className="p__opensans">
-				<a href="#menu">Menu</a>
-			</li>
-			<li className="p__opensans">
-				<a href="#contact">Contactez</a>
-			</li>
-		</ul>
-		<div className="app__navbar-login">
-			<a href="#login" className="p__opensans">
-				Se connecter / Créer un compte
-			</a>
-			<div />
-			<a href="/" className="p__opensans">
-				réserver une table
-			</a>
-		</div>
-		<div className="app__nabvar-smallscreen">
-			<GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}} />
-			<div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
-				<MdOutlineRestaurantMenu
-					fontSize={27}
-					className="overlay__close"
-					onClick={() => {}}
-				/>
-			<ul className="app__navbar-smallscreen-links">
+const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
+
+	return (
+		<nav className="app__navbar">
+			<div className="app__navbar-title">
+				<h1>Le COURTOISVILLE</h1>
+			</div>
+			<ul className="app__navbar-links">
 				<li className="p__opensans">
 					<a href="#home">Accueil</a>
 				</li>
@@ -56,9 +25,35 @@ const Navbar = () => (
 					<a href="#contact">Contactez</a>
 				</li>
 			</ul>
+			<div className="app__navbar-login">
+				<a href="#login" className="p__opensans">
+					Se connecter / Créer un compte
+				</a>
+				<div />
+				<a href="/" className="p__opensans">
+					réserver une table
+				</a>
 			</div>
-		</div>
-	</nav>
-);
+
+			<div className="app__navbar-smallscreen">
+				<GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)} />
+			
+        {toggleMenu && (
+          <div className="app__navbar-smallscreen_overlay flex__center slide-bottom">
+            <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
+            <ul className="app__navbar-smallscreen_links">
+              <li><a href="#home" onClick={() => setToggleMenu(false)}>Accueil</a></li>
+              <li><a href="#about" onClick={() => setToggleMenu(false)}>Présentation</a></li>
+              <li><a href="#menu" onClick={() => setToggleMenu(false)}>Menu</a></li>
+              <li><a href="#contact" onClick={() => setToggleMenu(false)}>Contactez</a></li>
+            </ul>
+          </div>
+        )}
+
+
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
-
-import './Header.css';
+import React from "react";
+import { images } from "../../constants";
+import "./Header.css";
 
 const Header = () => (
-  <div>
-    Header
-  </div>
+	<div className="app__header app__wrapper section__padding" id="home">
+		<div className="app__wrapper_info">
+      
+    </div>
+		
+    <div className="app__wrapper_img">
+
+    </div>
+	</div>
 );
 
 export default Header;

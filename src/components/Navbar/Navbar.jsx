a6990298357b7dad8454f7bc1f4d8c2fd1dd4dd3
robcabobb <robcabobb@gmail.com>
2022-05-13 11:16:15 -0400

    added nav elements

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import {GiHamburgerMenu} from 'react-icons/hi'
+import {MdOutlineRestaurantMenu} from 'react-icons/md'
+
+import images from '../../constants/images'
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
+  <nav className='app__navbar'>
+    <div className='app__navbar-logo'>
+      <img src={images.gericht} alt="app logo"/>
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'><a href="#home">Accueil</a></li>
+      <li className='p__opensans'><a href="#about">Présentation</a></li>
+      <li className='p__opensans'><a href="#menu">Menu</a></li>
+      <li className='p__opensans'><a href="#contact">Contactez</a></li>
+    </ul>
+  </nav>
 );
 
 export default Navbar;
